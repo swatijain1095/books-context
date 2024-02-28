8499d7a274491612c74706d0005e7895f8501708
Swati Jain <jain.swati71@gmail.com>
2024-02-28 19:42:41 +0100

    Added updating title functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import BookList from "./components/BookList";
 function App () {
     const [books, setBooks] = useState([]);
     
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id){
+                return { ...book, title: newTitle };
+            }
+            return book;
+        });
+        setBooks(updatedBooks);
+    };
+
     const deleteBookById = (id) => {
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
@@ -20,7 +30,7 @@ function App () {
         setBooks(updatedBooks);
     }
     return <div className="app">
-        <BookList books={books} onDelete={deleteBookById}/>
+        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
         <BookCreate onCreate={handleCreateBook}/>
     </div>;
 };
