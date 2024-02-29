fe0831c800e12e7fe18de69cc7cd5b790239b5b9
Swati Jain <jain.swati71@gmail.com>
2024-02-29 22:08:20 +0100

    Added functionality related to API requests and created new record and fetch list of records using REST client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App () {
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async() => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
     
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
@@ -22,10 +33,15 @@ function App () {
         setBooks(updatedBooks);
     };
 
-    const handleCreateBook = (title) => {
-        const updatedBooks = [...books, 
-            {id: Math.round(Math.random() * 9999),
-             title}];
+    const handleCreateBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
+        const updatedBooks = [
+            ...books,
+            response.data 
+            ];
 
         setBooks(updatedBooks);
     }
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -14,7 +14,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
     const handleSubmit = (id, newTitle) => {
         setShowEdit(false);
-        onEdit(id, newTitle, );
+        onEdit(id, newTitle );
     }
 
     let content = <h3>{book.title}</h3>;
