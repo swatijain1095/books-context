3c46077ea17842d8216518b305ce88e7cc1610c5
Swati Jain <jain.swati71@gmail.com>
2024-03-01 16:14:42 +0100

    Added refactoring and used some Reusable hooks

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,9 +1,9 @@
-import { useState, useContext } from "react";
-import BooksContext from "../context/books";
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookCreate() {
     const [title, setTitle] = useState('');
-    const { handleCreateBook } = useContext(BooksContext);
+    const { handleCreateBook } = useBooksContext();
 
     const handleChange = (event) => {
         setTitle(event.target.value);
